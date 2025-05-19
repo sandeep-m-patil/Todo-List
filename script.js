54cfbc97ea6a7f547f30b41d58e5cfbbc5e6c679
sandeep <sandeeppatil10705@gmail.com>
2025-05-19 11:20:29 +0530

    added stylings

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ function renderTodoList() {
         const todoObject = todoList[i];
         const { name, dueDate } = todoObject;
         const html =
-            `<p>
-        ${name}  ${dueDate}
+            `
+       <div> ${name} </div><div> ${dueDate}</div>
         <button onclick="todoList.splice(${i},1);
-         renderTodoList();">Delete</button>
-        </p> `;
+         renderTodoList();" class="delete-todo-btn">Delete</button>
+         `;
         todoListHTML += html;
 
     }
@@ -38,10 +38,9 @@ function addTodo() {
     const todoDueDate = document.getElementById('dueDate').value;
 
     todoList.push({
-        name:todoInput,
-        dueDate:todoDueDate
+        name: todoInput,
+        dueDate: todoDueDate
     });
-    console.log(todoList);
     todoInput.value = '';
     renderTodoList();
 }
